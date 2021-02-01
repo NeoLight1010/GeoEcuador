bd49fffbcba4527d31242c5951ee7107689ee2e9
Anthony Suárez <neolight1010@gmail.com>
2021-02-01 18:41:45 -0500

    Added close button for InfoDisplay.

--- src/displays/info_display/info_display.gd
+++ src/displays/info_display/info_display.gd
@@ -3,13 +3,22 @@ extends Panel
 onready var province_name = get_node("/root/Main").params["InfoDisplay"]["province_name"]
 onready var data = get_node("/root/Main").params["InfoDisplay"]["data"]
 
+onready var Header = $MainVBox/Header
+onready var Body = $MainVBox/Body
+
 onready var Title = $MainVBox/Header/Center/Title
-onready var Capital = $MainVBox/Body/DataVBox/Capital
-onready var Description = $MainVBox/Body/DataVBox/Description
-onready var ImageNode = $MainVBox/Body/DataVBox/Image
+onready var Capital = Body.get_node("Scroll/DataVBox/Capital")
+onready var Description = Body.get_node("Scroll/DataVBox/Description")
+onready var ImageNode = Body.get_node("Scroll/DataVBox/Image")
 
 func _ready():
 	Title.text = province_name
 	Capital.text = "Capital: " + data['Capital']
 	Description.text = Provincia.new().read_description_txt(data['Descripción'])
 	ImageNode.texture = Provincia.new().load_image(data['Imagen'])
+
+
+func _on_Button_pressed():
+	var Main = $"/root/Main"
+	
+	Main.change_scene("MapDisplay")
